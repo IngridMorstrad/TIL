a36145b5c978258ed620bf93adf69bd023f1541e
IngridMorstrad <ashwin91+git@gmail.com>
2023-03-20 18:34:17 -0700

    Fix const list and MIL filename

--- documents/.vuepress/const.js
+++ documents/.vuepress/const.js
@@ -1,16 +1,4 @@
 exports.NetworkList = ["Network/ssh-tunneling"];
 exports.MILList = [
-  "MIL/mil-2108.md",
-  "MIL/mil-2107.md",
-  "MIL/mil-2106.md",
-  "MIL/mil-2106.md",
-  "MIL/mil-2104.md",
-  "MIL/mil-2103.md",
-  "MIL/mil-2102.md",
-  "MIL/mil-2101.md",
-  "MIL/mil-2011-2012.md",
-  "MIL/mil-2010.md",
-  "MIL/mil-2009.md",
-  "MIL/mil-2008.md",
-  "MIL/mil-1911.md",
+  "MIL/mil-2303.md"
 ];
